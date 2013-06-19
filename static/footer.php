09821fe571dff1d837ed5b0b9ac6f5e5d929ad5d
Thomas Lynge <tlynge@gmail.com>
2013-06-19 12:04:38 +0200

    Replaced primo.rasmusdamberg.dk and primo.danskkulturarv.dk with new primo url.

--- static/footer.php
+++ static/footer.php
@@ -4,38 +4,38 @@
 	<div class="container">
 		<div class="box">
 			<h3>Biblioteket</h3>
-				<span><a href="http://primo.danskkulturarv.dk/static/indhold.php">Indhold i biblioteket</a></span>
-				<span><a href="http://primo.danskkulturarv.dk/static/ombiblioteket.php">Om biblioteket</a></span>
-				<span><a href="http://primo.danskkulturarv.dk/static/saadanlaanerdu.php">Sådan låner du</a></span>
-				<span><a href="http://primo.danskkulturarv.dk/static/retningslinjer.php">Retningslinjer for lån</a></span>
+				<span><a href="http://77.66.32.238/php/intern/arkiv/primo/static/indhold.php">Indhold i biblioteket</a></span>
+				<span><a href="http://77.66.32.238/php/intern/arkiv/primo/static/ombiblioteket.php">Om biblioteket</a></span>
+				<span><a href="http://77.66.32.238/php/intern/arkiv/primo/static/saadanlaanerdu.php">Sådan låner du</a></span>
+				<span><a href="http://77.66.32.238/php/intern/arkiv/primo/static/retningslinjer.php">Retningslinjer for lån</a></span>
 				<span><a target="_blank" href="https://aleph-01.kb.dk/F/?func=file&file_name=bor-new&local_base=DRR50">Ny låner</a></span>
-				<span><a href="http://primo.danskkulturarv.dk/static/soegetips.php">Søgetips</a></span>
-				<span><a href="http://primo.rasmusdamberg.dk/static/foreslaa.php" href="#">Foreslå indkøb</a></span>
+				<span><a href="http://77.66.32.238/php/intern/arkiv/primo/static/soegetips.php">Søgetips</a></span>
+				<span><a href="http://77.66.32.238/php/intern/arkiv/primo/static/foreslaa.php" href="#">Foreslå indkøb</a></span>
 		</div>
 		<div class="box">
 			<h3>DR databaser</h3>
-				<span><a target="_blank" href="http://wfe/digi">Mediearkivet <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://oas.net.dr.dk:9910/musa/drmusa.vis">MUSA <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://inlineweb/musikmedier_daletsoeg/">Musikserveren <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://brs01/netahtml/pressefoto.htm">Fotografier <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://brs01/netahtml/lyde.htm">Lydeffekter <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://wfe/digi">Mediearkivet <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://oas.net.dr.dk:9910/musa/drmusa.vis">MUSA <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://inlineweb/musikmedier_daletsoeg/">Musikserveren <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://brs01/netahtml/pressefoto.htm">Fotografier <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://brs01/netahtml/lyde.htm">Lydeffekter <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
 		</div>
 		<div class="box">
 			<h3>Eksterne databaser</h3>
-				<span><a target="_blank" href="http://apps.infomedia.dk/ms3/Login.aspx?i=09D8884F-F2D3-441D-90B0-D260B0530D33">Infomedia <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://ordbog.gyldendal.dk/">Gyldendals Røde Ordbøger <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://www.faktalink.dk/">Faktalink <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://abon.greens.dk/Online/default.aspx">Greens Online <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Tekst/20090109102824.htm">Se alle<img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://apps.infomedia.dk/ms3/Login.aspx?i=09D8884F-F2D3-441D-90B0-D260B0530D33">Infomedia <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://ordbog.gyldendal.dk/">Gyldendals Røde Ordbøger <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://www.faktalink.dk/">Faktalink <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://abon.greens.dk/Online/default.aspx">Greens Online <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Tekst/20090109102824.htm">Se alle<img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
 		</div>
 		<div class="box">
 			<h3>Få viden om</h3>
-				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Tekst/20090109103308.htm">Avissamlinger <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Tekst/20090109103005.htm">Tidsskriftoversigt <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Foto/20090109103855.htm">Fotosamlinger <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Research-pakker/20110817094051.htm">Researchpakker <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Portraetgalleri/20110817094051.htm">Portrætgalleri <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
-				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter%20og%20ydelser/Arkiv%20og%20research/Arkivperler/20110817094051.htm">Arkivperler <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Tekst/20090109103308.htm">Avissamlinger <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Tekst/20090109103005.htm">Tidsskriftoversigt <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Foto/20090109103855.htm">Fotosamlinger <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Research-pakker/20110817094051.htm">Researchpakker <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Portraetgalleri/20110817094051.htm">Portrætgalleri <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
+				<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter%20og%20ydelser/Arkiv%20og%20research/Arkivperler/20110817094051.htm">Arkivperler <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
 		</div>
 		<div class="box last">
 			<h3>Kontakt DR Arkiv & Research</h3>
@@ -46,7 +46,7 @@
 			Mandag til fredag <br />
 			kl.: 10:00 - 17:00</p>
 			
-			<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Arkiv+og+research+forside.htm">A&R på DRinde <img src="http://primo.danskkulturarv.dk/images/external.gif" alt=""></a></span>
+			<span><a target="_blank" href="http://cmsil/Produktion/Faciliteter+og+ydelser/Arkiv+og+research/Arkiv+og+research+forside.htm">A&R på DRinde <img src="http://77.66.32.238/php/intern/arkiv/primo/images/external.gif" alt=""></a></span>
 		</div>
 	</div>
 </div>
