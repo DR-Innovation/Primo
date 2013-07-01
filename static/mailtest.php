1f29d92398d816f12e4aa3959d5aaa6902043fc6
Thomas Lynge <tlynge@gmail.com>
2013-07-01 12:38:07 +0200

    Netgroup mailform.

--- static/mailtest.php
+++ static/mailtest.php
@@ -1,12 +1,21 @@
 <?php
-echo("Mail Test");
+echo("Mail Test. ");
 
-$to = "thfl@dr.dk";
-$subject = "Test mail";
-$message = "Hello! This is a simple email message.";
-$from = "thfl@dr.dk";
-$headers = "From:" . $from;
-mail($to,$subject,$message,$headers);
-echo "Mail Sent.";
+//$to = "thfl@dr.dk";
+//$subject = "Test mail";
+//$message = "Hello! This is a simple email message.";
+//$from = "thfl@dr.dk";
+//$headers = "From:" . $from;
+//mail($to,$subject,$message,$headers);
+//echo "Mail Sent.";
+
+$Name = "Thomas";
+$email = "thfl@dr.dk"; //senders e-mail adress 
+$recipient = "tlynge@gmail.com"; //recipient 
+$mail_body = "The text for the mail..."; //mail body 
+$subject = "Subject for reviever"; //subject 
+$header = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields 
+mail($recipient, $subject, $mail_body, $header); //mail command :) 
+echo "Mail Sent...";
 
 ?>
